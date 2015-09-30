086474fb3b441904aa446d44489e48737039b355
Lew Parker <parker@earthvectors.com>
2015-09-29 22:15:09 -0600

    Add .iex.esx for setting up IEx sessions

---
+++ diving_into_ecto/fishing_spot/.iex.exs
@@ -0,0 +1,15 @@
+alias FishingSpot.Repo
+alias FishingSpot.Location
+alias FishingSpot.LocationTrip
+alias FishingSpot.LocationType
+alias FishingSpot.Fisherman
+alias FishingSpot.PersonTrip
+alias FishingSpot.FishLanded
+alias FishingSpot.FishSpecies
+alias FishingSpot.FlyType
+alias FishingSpot.Trip
+alias FishingSpot.Queries
+
+import Ecto.Query
+
+q = fn -> c("lib/queries.exs") end
